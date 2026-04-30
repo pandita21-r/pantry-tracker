f6b05718bab0f9743f964fc839105e56087200e6
Raymark Pandita <raymarkpandita21@gmail.com>
2026-04-30 15:38:33 +0800

    Fixed application file

--- backend/src/main/java/com/teamBangan/pantrytracker/PantryTrackerApplication.java
+++ backend/src/main/java/com/teamBangan/pantrytracker/PantryTrackerApplication.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package com.teamBangan.pantrytracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PantryTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		SpringApplication.run(PantryTrackerApplication.class, args);
 
+	}
+	
 }
